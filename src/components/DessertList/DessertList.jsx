baa9ad1eb62afce5674b11db2c9091176e79ebc9
Kelly Popko <kelpopko@gmail.com>
2025-09-15 16:49:15 -0500

    [wip] Add functionality to DessertList

--- src/components/DessertList/DessertList.jsx
+++ src/components/DessertList/DessertList.jsx
@@ -1,4 +1,11 @@
+import { useState } from 'react'
+
 const DessertList = () => {
+  const [data, setData] = useState()
+  const [error, setError] = useState()
+  const [loading, setLoading] = useState()    // how do we know to use these?
+  // When loading, the component should print htat it is loading
+
   return (
     <div>
       <h1>Desserts</h1>
