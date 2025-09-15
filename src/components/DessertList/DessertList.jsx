5d8e56e86978c7b827d10ff8bce477d9a93f1bc5
Kelly Popko <kelpopko@gmail.com>
2025-09-15 16:49:15 -0500

    Add skeleton functionality to DessertList
    - Fetch dessert list information
    - placeholder display (i.e. as the JSON)
    - Updating setData, setLoading, setError

--- src/components/DessertList/DessertList.jsx
+++ src/components/DessertList/DessertList.jsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from 'react'
+
 const DessertList = () => {
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+
+      try {
+        const response = await fetch ('/data/data.json')
+        if (!response.ok) {
+          throw new Error(`File not found! URL: ${response.url}`)
+        }
+        const data = await response.json()
+        setData(data)
+      } catch (error) {
+        console.log(error)
+        setError(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  const desserts = JSON.stringify(data)
+
+  if (loading) {
+    return <p>loading</p>
+  }
+
+  if (error) {
+    return <p>{error}</p>
+  }
+
   return (
     <div>
       <h1>Desserts</h1>
+      {desserts}
     </div>
   )
 }
